2cfb4989800ca9b2d1599a70b1147e201492cae3
Ubuntu <ubuntu@ip-172-31-20-255.ap-south-1.compute.internal>
2026-07-08 08:27:25 +0000

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const http = require("http");
+const client = require("prom-client");
 
-const server = http.createServer((req, res) => {
-    res.end("Hello from Jenkins CI/CD Pipeline");
+// Default system/application metrics collect karega
+client.collectDefaultMetrics();
+
+// Saare metrics yahan store honge
+const register = client.register;
+
+
+const server = http.createServer(async (req, res) => {
+
+    // Prometheus yahan se metrics lega
+    if (req.url === "/metrics") {
+
+        res.writeHead(200, {
+            "Content-Type": register.contentType
+        });
+
+        res.end(await register.metrics());
+
+    } 
+    
+    // Normal application response
+    else {
+
+        res.end("Hello from Jenkins CI/CD Pipeline");
+
+    }
+
 });
+
 
 server.listen(3000, () => {
     console.log("Server running on port 3000");
 });
-
