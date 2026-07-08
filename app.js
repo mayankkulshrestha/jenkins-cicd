0f3cecd4a8fe04404df88308345f0c17bd75c0a2
Ubuntu <ubuntu@ip-172-31-20-255.ap-south-1.compute.internal>
2026-07-08 08:47:16 +0000

    Expose node app metrics to prometheus

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ const server = http.createServer(async (req, res) => {
 
 });
 
-
-server.listen(3000, () => {
+server.listen(3000, "0.0.0.0", () => {
     console.log("Server running on port 3000");
 });
